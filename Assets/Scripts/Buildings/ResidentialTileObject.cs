4fa135254ffd460f7f56ddd45b7b4cc58bd8d162
MihirKulgod <139000075+MihirKulgod@users.noreply.github.com>
2026-02-23 20:25:52 +0000

    Added lights for Neighbourhood, Park, Coal Plant. Other buildings need reimporting for intended effects

--- Assets/Scripts/Buildings/ResidentialTileObject.cs
+++ Assets/Scripts/Buildings/ResidentialTileObject.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Drawing;
-using System.Security.Cryptography;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class ResidentialTileObject : TileObject
 {
-    public float occupancy = 0; // The occupancy at this current moment for this particular TileObject, definition has the max occupancy
+    [NonSerialized] public float occupancy = 0; // The occupancy at this current moment for this particular TileObject, definition has the max occupancy
 
     private void OnDestroy()
     {
